373963825c0652dcbd49bf074524a4c4c45e20d9
racewalkWill <whlb25@gmail.com>
2021-06-02 12:06:29 -0500

    N74.02 Changes in internal names to WillsFilterTool. cloudKit datastore renamed  to iCloud.L-BSoftwareArtist.WillsFilterTool. Pushed schema to iCloud oneTime

--- Surreality/AppDelegate.swift
+++ Surreality/AppDelegate.swift
@@ -34,6 +34,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        //******* ONLY One time to push schema to cloudKit
+        // get the store description
+//        guard let description = PersistentContainer.persistentStoreDescriptions.first else {
+//            fatalError("Could not retrieve a persistent store description.")
+//        }
+        // initialize the CloudKit schema
+
+
+//        do {
+//            try
+//            PersistentContainer.initializeCloudKitSchema(options: NSPersistentCloudKitContainerSchemaInitializationOptions.printSchema)
+//        }
+//            catch {
+//                NSLog("initializeCloudKitSchema \(error.localizedDescription)" )  }
+
 
 
 //       PGLFaceCIFilter.register()
--- Surreality/StoredData support/PGL-CDFilterStack.swift
+++ Surreality/StoredData support/PGL-CDFilterStack.swift
@@ -13,7 +13,7 @@ import CoreImage
 import Photos
 import PhotosUI
 import os
-var PersistentContainer: NSPersistentContainer = {
+var PersistentContainer: NSPersistentCloudKitContainer = {
        /*
         The persistent container for the application. This implementation
         creates and returns a container, having loaded the store for the
@@ -21,7 +21,14 @@ var PersistentContainer: NSPersistentContainer = {
         error conditions that could cause the creation of the store to fail.
        */
 //       let container = NSPersistentContainer(name: "Surreality")
-    let container = NSPersistentCloudKitContainer(name: "Surreality")
+      let container = NSPersistentCloudKitContainer(name: "WillsFilterTool")
+
+  
+
+    //******* ONLY One time END
+
+
+//    let container = NSPersistentContainer(name: "WillsFilterTool")
        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
            if let error = error as NSError? {
                // Replace this implementation with code to handle the error appropriately.
