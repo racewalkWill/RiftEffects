5f81434d9af912bf0481ec2faf06ec47893d21d9
racewalkWill <whlb25@gmail.com>
2023-12-08 12:58:38 -0600

    R138.1  PGLCameraInterface, PGLCameraViewFilter, PGLCopyOutCIFilter added. Not working yet.

---
+++ RiftEffects/Controllers/VideoCamFilter/PGLCameraInterface.swift
@@ -0,0 +1,9 @@
+//
+//  PGLCameraInterface.swift
+//  RiftEffects
+//
+//  Created by Will on 12/8/23.
+//  Copyright © 2023 Will Loew-Blosser. All rights reserved.
+//
+
+import Foundation
---
+++ RiftEffects/Controllers/VideoCamFilter/PGLCameraViewFilter.swift
@@ -0,0 +1,9 @@
+//
+//  PGLCameraViewFilter.swift
+//  RiftEffects
+//
+//  Created by Will on 12/7/23.
+//  Copyright © 2023 Will Loew-Blosser. All rights reserved.
+//
+
+import Foundation
